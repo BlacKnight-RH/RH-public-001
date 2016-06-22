59bc95dd3ab19123b283f7b2476085c5de0f0b23
Black Knight <namikaze_minatoo@alexu.edu.eg>
2016-06-22 22:09:22 +0200

    Update Master.java

--- proj4-debugging/src/Master.java
+++ proj4-debugging/src/Master.java
@@ -5,25 +5,25 @@ public class Master extends Employee
 {       
 
 	
-	public void add_master()                        // Create Main Method For this class - now we can use < for > loop without errors 
+	public void add_master()                                                                         // Create Main Method For this class - now we can use < for > loop without errors 
 	{
 		int num ;
-		String user_data = new String();                           // < str >  Changed to < user_data > 
+		String user_data = new String();                                                          // < str >  Changed to < user_data > 
 		
-		Scanner user_input = new Scanner(System.in);               // < in >  Changed to < user_input >
+		Scanner user_input = new Scanner(System.in);                                               // < in >  Changed to < user_input >
 	
 		
-		Employee [] ar = new  Employee[5] ;                       // Redeclare new array of objects < ar > inside < main > Method 
+		Employee [] ar = new  Employee[5] ;                                                        // Redeclare new array of objects < ar > inside < main > Method 
 		Workshop worsh = new Workshop() ;
 		
 		
 		
 		
-		for (int i = 0; i < ar.length ; i++)                      // it's Recommended to depend on array length instead of using static Number  
+		for (int i = 0; i < ar.length ; i++)                                                          // it's Recommended to depend on array length instead of using static Number  
 		{
 			ar[i] = new Employee();
 			
-			System.out.println("Enter a String val without spaces ["+ (i + 1) +"] : ");         // give Message to the user that tells him what he should to do 
+			System.out.println("Enter a String val without spaces ["+ (i + 1) +"] : ");          // give Message to the user that tells him what he should to do 
 			user_data = user_input.next();
 			worsh.setName(user_data);
 			
